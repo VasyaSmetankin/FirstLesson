0fccac3ef18045c0290091d0e8cefbfe9329d92c
a <a>
2022-09-18 19:48:59 +0300

    logic creation

--- FirstLesson/ViewController.swift
+++ FirstLesson/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingButton: UIButton!
+    @IBOutlet var greetingLabel: UILabel!
+    var count = 0
     override func viewDidLoad() {
+        greetingLabel.isHidden.toggle()
+        //greetingButton.layer.cornerRadius = 10
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func GreetingButtonDidTapped() {
+        count += 1
+        print("greeting button did tap \(count) times")
+        greetingLabel.isHidden.toggle()
+        greetingButton.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal)
 
+        
+    }
+    
 }
 
